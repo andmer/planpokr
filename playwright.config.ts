cc422dface73cf7acdd081d3f62a63537caf319e
Andre Mermegas <andmer@gmail.com>
2026-05-13 20:46:51 -0400

    test: bootstrap vitest + playwright

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'tests/e2e',
+  webServer: {
+    command: 'pnpm preview --port 4173',
+    port: 4173,
+    reuseExistingServer: !process.env.CI
+  },
+  use: { baseURL: 'http://localhost:4173' }
+});
